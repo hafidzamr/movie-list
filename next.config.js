6c3d68361c0b5ee498b32d397181c34be894744e
Hafidz <hafidzdev@gmail.com>
2021-05-29 03:11:08 +0700

    test terusss

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? 'https://unruffled-hawking-8f4248.netlify.app/' : '',
   target: 'serverless',
   images: {
     domains: ['image.tmdb.org']
